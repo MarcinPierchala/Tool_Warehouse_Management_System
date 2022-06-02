f700cd702a24f88a630ab705f14a38cb1134de36
MarcinPierchala <macintecmarcin@gmail.com>
2022-06-02 20:46:51 +0200

    createdDB_ServerAndConnectionConfiguration

---
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System.DataAccess/ToolWarehouseStorageContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tool_Warehouse_Management_System.DataAccess.Entities;
+
+namespace Tool_Warehouse_Management_System.DataAccess
+{
+    public class ToolWarehouseStorageContext : DbContext
+    {
+        public ToolWarehouseStorageContext(DbContextOptions<ToolWarehouseStorageContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Tool> Tools { get; set; }
+    }
+}
---
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System.DataAccess/ToolWarehouseStorageContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool_Warehouse_Management_System.DataAccess
+{
+    public class ToolWarehouseStorageContextFactory : IDesignTimeDbContextFactory<ToolWarehouseStorageContext>
+    {
+        private const string ConnectionString = "Data Source=DESKTOP-RRLI5LA\\TOOLWARDATABASE;Initial Catalog=ToolWarehouseStorage;Integrated Security=True";
+
+        public ToolWarehouseStorageContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ToolWarehouseStorageContext>();
+            optionsBuilder.UseSqlServer(ConnectionString);
+            return new ToolWarehouseStorageContext(optionsBuilder.Options);
+        }
+    }
+}
