095c05e816d64b9ee6213d61a0a89023f7e39a5c
MarcinPierchala <macintecmarcin@gmail.com>
2022-06-02 21:18:09 +0200

    Added_validation_2_Tool_table_and_Updated_DB

---
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System.DataAccess/Entities/EntityBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tool_Warehouse_Management_System.DataAccess.Entities
+{
+    public class EntityBase
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
--- Tool_Warehouse_Management_System/Tool_Warehouse_Management_System.DataAccess/Entities/Tool.cs
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System.DataAccess/Entities/Tool.cs
@@ -1,15 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tool_Warehouse_Management_System.DataAccess.Entities
 {
-    public class Tool
+    public class Tool :EntityBase
     {
-        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Category { get; set; }
         public int YearProduction { get; set; }
     }
