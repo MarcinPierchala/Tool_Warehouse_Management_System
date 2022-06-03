18df5504ffa9d4d7656e2582f2ea2080d0271e55
MarcinPierchala <macintecmarcin@gmail.com>
2022-06-03 16:46:36 +0200

    createdGenericRepositoryFromDependencyInjection

---
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System.DataAccess/IRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tool_Warehouse_Management_System.DataAccess.Entities;
+
+namespace Tool_Warehouse_Management_System.DataAccess
+{
+    public interface IRepository<T> where T : EntityBase
+    {
+        IEnumerable<T> GetAll();
+
+        T GetById(int id);
+
+        void Insert(T entity);
+
+        void Update(T entity);
+
+        void Delete(int id);
+    }
+}
+
+//w Setup, w configureServices l:29 :
+//services.AddScoped(typeof(IRepository<>),typeof(Repository<>));
+
--- Tool_Warehouse_Management_System/Tool_Warehouse_Management_System/Startup.cs
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System/Startup.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tool_Warehouse_Management_System.DataAccess;
 
 namespace Tool_Warehouse_Management_System
 {
@@ -26,6 +27,7 @@ namespace Tool_Warehouse_Management_System
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
