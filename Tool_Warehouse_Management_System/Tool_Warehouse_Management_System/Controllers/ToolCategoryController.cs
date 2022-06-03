735b3e1e3fcace3d49a1e546fc2a64a53de64adf
MarcinPierchala <macintecmarcin@gmail.com>
2022-06-03 21:02:43 +0200

    createdToolWarehouseController_itS_Working

---
+++ Tool_Warehouse_Management_System/Tool_Warehouse_Management_System/Controllers/ToolCategoryController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tool_Warehouse_Management_System.DataAccess;
+using Tool_Warehouse_Management_System.DataAccess.Entities;
+
+namespace Tool_Warehouse_Management_System.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToolCategoryController : ControllerBase
+    {
+        private readonly IRepository<ToolCategory> toolCategoryRepository;
+        public ToolCategoryController(IRepository<ToolCategory> toolCategoryRepository)
+        {
+            this.toolCategoryRepository = toolCategoryRepository;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IEnumerable<ToolCategory> GetAllTools() => this.toolCategoryRepository.GetAll();
+
+        [HttpGet]
+        [Route("{toolCategoryId}")]
+        public ToolCategory GetToolCategoryById(int toolCategoryId) => this.toolCategoryRepository.GetById(toolCategoryId);
+    }
+}
